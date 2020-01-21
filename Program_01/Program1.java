17686b627af26d0a353247ef41fc2308252bb7c2
GoodGuyGregory <greg.witt625@gmail.com>
2020-01-21 17:50:47 -0500

    adds notes on Memory and JIT Compiler

--- Program_01/Program1.java
+++ Program_01/Program1.java
@@ -4,6 +4,9 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 public class Program1 {
+
+    private static final int MegaBytes = 10241024;
+
     public static void main(String[] args) {
         // Prints Hard Coded Name:
         String name = "Greg Witt";
@@ -21,21 +24,28 @@ public class Program1 {
         // Java Version:
         String JavaVersion = System.getProperty("java.version");
         System.out.println("Java Version: " + JavaVersion);
-
         String JavaVendor = System.getProperty("java.vendor");
         System.out.println("Java Vendor: " + JavaVendor);
-
+        // JVM:
         System.out.println("=================================");
+        // Constants from JVM
+        long FreeMemory = Runtime.getRuntime().freeMemory() / MegaBytes;
+        long TotalMemory = Runtime.getRuntime().totalMemory() / MegaBytes;
+        long MaxMemory = Runtime.getRuntime().maxMemory() / MegaBytes;
 
+        System.out.println("Java Virtual Machine Properties:");
+        System.out.println("Max Memory:" + MaxMemory + "mbs");
+        System.out.println("Free Memory:" + FreeMemory + "mbs");
+        System.out.println("Used Memory: " + (MaxMemory - FreeMemory) + "mbs");
+        System.out.println("Total Memory:" + TotalMemory + "mbs");
         System.out.println("=================================");
         // Operating System Architecture:
         System.out.println("Operating System Architecture:");
         String OsName = System.getProperty("os.name");
         String OsVersion = System.getProperty("os.version");
-
         System.out.println("Operating System: " + OsName);
         System.out.println("Operating System Version: " + OsVersion);
-
+        // Working Directory:
         System.out.println("=================================");
         String UserWorkingDir = System.getProperty("user.dir");
         System.out.println("User Working Directory: " + UserWorkingDir);
--- Program_01/Running.md
+++ Program_01/Running.md
@@ -13,3 +13,23 @@
 **Free Memory in the Java Runtime JVM**
 
 [Link to Calls](https://javarevisited.blogspot.com/2012/01/find-max-free-total-memory-in-java.html)
+
+**Free Memory in Java**
+
+Java free memory can be returned from an object method below, the method will return the size in bytes of the device.
+
+`Runtime.getRuntime().freeMemory();`
+
+calling the garbage collector will result in slightly larger memory with
+
+`System.gc()` or `Runtime.gc()` this will result in slightly higher free memory reclaimed by dead objects.
+
+**Total Memory** 
+
+calling the *Runtime* object again can produce the total memory from the JVM, this represents current heapsize. which is itself a combination of free memory and combination of used memory
+
+`Runtime.getRuntim.totalMemory();`
+
+**Just In Time Compiler**
+
+[Checking if The JIT is Enabled](https://www.ibm.com/support/knowledgecenter/SSYKE2_8.0.0/com.ibm.java.vm.80.doc/docs/jit.html)
