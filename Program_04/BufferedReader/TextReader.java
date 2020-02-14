242b9a2bd84492699eb0174a089831c190ec47dd
GoodGuyGregory <greg.witt625@gmail.com>
2020-02-14 02:48:46 -0500

    adds notes and TextReader Program

---
+++ Program_04/BufferedReader/TextReader.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TextReader {
+    public static void main(String[] args) throws IOException {
+        File f = new File("../infile.txt"); // Creation of File Descriptor for input file
+
+        FileReader fr = new FileReader(f); // Creation of File Reader object
+        BufferedReader br = new BufferedReader(fr); // Creation of BufferedReader object
+        int c = 0;
+        while ((c = br.read()) != -1) // Read char by Char
+        {
+            char character = (char) c;
+            switch (character) {
+            case 'a':
+                // Call Cub_1.put()
+                System.out.println('a');
+                break;
+            case 'e':
+                // Call Cub_1.put()
+                System.out.println('e');
+                break;
+            case 'i':
+                // Call Cub_1.put()
+                System.out.println('i');
+                break;
+            case 'o':
+                // Call Cub_1.put()
+                System.out.println('o');
+                break;
+            case 'u':
+                // Call Cub_1.put()
+                System.out.println('u');
+                break;
+            default:
+                // Call Cub_2.put()
+                System.out.println("Other Letter");
+                ;
+            }
+            // System.out.print(character);
+        }
+
+        // File Done Reading Put a Period in the Cubbyhole:
+
+    }
+}
---
+++ Program_04/notes.md
@@ -0,0 +1,5 @@
+**Methods for Reading and Writing to files**
+
+[Geeks for Geeks](https://www.geeksforgeeks.org/different-ways-reading-text-file-java/)
+
+[Number of Words Counter Program](https://www.candidjava.com/tutorial/java-program-to-count-number-of-words-in-a-file/)
