dc7928ec1cff6802a373cac9659ce417c6d4e574
GoodGuyGregory <greg.witt625@gmail.com>
2020-04-29 15:17:37 -0400

    adds program10

---
+++ Program_10/Program10.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class P10 {
+
+    public void interrogateDir(String dirString, String filename) {
+        File folder = new File(dirString);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            String fileTemp = listOfFiles[i].getName().toString();
+            if (listOfFiles[i].isFile() && fileTemp.equals(filename)) {
+                System.out.println("Directory: \"" + listOfFiles[i] + "\" --- contains: \"" + filename + "\"");
+                // break;
+            } // if
+            else {
+                // System.out.println("Directory "+listOfFiles[i]+" does not contain
+                // "+filename);
+            }
+        } // for
+    }// listFiles
+
+    public static void main(String[] args) {// main
+
+        P10 obj = new P10();
+        String cmdLineArg = args[0];
+        // String cmdLineArg2 = args[1];
+        // System.out.println("Command line argument is " + cmdLineArg);
+        // System.out.println("Command line argument 2 is " + cmdLineArg2);
+        String pathVar = System.getenv("PATH");
+        // System.out.println("The PATH variable is " + pathVar);
+        String[] dirArray = pathVar.split(":");
+        for (String eachDir : dirArray) {
+            // System.out.println(eachDir);
+            obj.interrogateDir(eachDir, cmdLineArg);
+        } // foreach
+
+    }// main
+}// class
