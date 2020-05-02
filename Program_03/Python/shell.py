d889e4a25a2ba01a08c4e420114611224d35f625
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-02 19:51:36 -0400

    adds logic stdin logic

--- Program_03/Python/shell.py
+++ Program_03/Python/shell.py
@@ -1,22 +1,26 @@
 import sys
 import os
 import fileinput
+import select
 
 
 def readTheDisk(commandArgs):
-    # diskDrive = []
-    counter = 0
-    print("Displaying Drive Data: ")
-    for line in sys.stdin:
-        #    Push to Structure to Trim
-        # str(line).strip('\n')
-        # diskDrive = diskDrive + [line[2:]]
-        if counter == 0 or counter == 1:
-            counter += 1
-        else:
 
-            sys.stdout.write(line[2:])
-            counter += 1
+    # Checking for Stdin Pipe
+    if select.select([sys.stdin], [], [], 0.0)[0]:
+        # diskDrive = []
+        counter = 0
+        print("Displaying Drive Data: ")
+        for line in sys.stdin:
+            #    Push to Structure to Trim
+            # str(line).strip('\n')
+            # diskDrive = diskDrive + [line[2:]]
+            if counter == 0 or counter == 1:
+                counter += 1
+            else:
+
+                sys.stdout.write(line[2:])
+                counter += 1
 
 
 def main():
