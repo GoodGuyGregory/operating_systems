7ef5baa80d3e020b84c126f912a9729b539c0925
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-02 22:44:06 -0400

    adds Parseexample

--- Program_03/Python/Playground/argParse/argParse.py
+++ Program_03/Python/Playground/argParse/argParse.py
@@ -4,7 +4,31 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
 # Two Types of args
-# Positional and Optional
-    parser.add_argument("number1", help="first number")
-    parser.add_argument("number2", help="second number")
+# Positional and Optional (--<var>)
+    parser.add_argument("--number1", help="first number")
+    parser.add_argument("--number2", help="second number")
+    parser.add_argument("--operation", help="operation",
+                        choices=["add", "subtract", "multiply"])
+    # Restrict operations with
+
     args = parser.parse_args()
+
+    # Args is an accessable object which can return commandline arguments
+    print(args.number1)
+    print(args.number2)
+    print(args.operation)
+
+    number1 = int(args.number1)
+    number2 = int(args.number2)
+    result = None
+
+    if args.operation == "add":
+        result = number1 + number2
+    elif args.operation == "subtract":
+        result = number1 - number2
+    elif args.operation == "multiply":
+        result = number1 * number2
+    else:
+        print("unsupported operation")
+
+    print("Result is: " + str(result))
