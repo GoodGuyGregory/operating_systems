d6c297a20363043d36602da15fd4a7e6599d932e
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-03 11:01:12 -0400

    adds filename args

--- Program_03/Python/shell.py
+++ Program_03/Python/shell.py
@@ -2,9 +2,22 @@ import sys
 import os
 import fileinput
 import select
+import argparse
+
+# declare parser class
+parser = argparse.ArgumentParser(add_help=False)
+
+parser.add_argument('-v', '-V', help="displays version", action="store_true")
+parser.add_argument('-h', '-H', '-?', action='help',
+                    default=argparse.SUPPRESS, help='show help and usage.')
+parser.add_argument('-f', '-F', type=str, help="file to be read")
+
+args = parser.parse_args()
 
 
 def readTheDisk(commandArgs):
+    # Checks for File to use
+    # if commandArgs
 
     # Checking for Stdin Pipe
     if select.select([sys.stdin], [], [], 0.0)[0]:
@@ -12,9 +25,6 @@ def readTheDisk(commandArgs):
         counter = 0
         print("Displaying Drive Data: ")
         for line in sys.stdin:
-            #    Push to Structure to Trim
-            # str(line).strip('\n')
-            # diskDrive = diskDrive + [line[2:]]
             if counter == 0 or counter == 1:
                 counter += 1
             else:
@@ -26,6 +36,9 @@ def readTheDisk(commandArgs):
 def main():
     commands = sys.argv
     readTheDisk(commands)
+    # Checking for files or Arguments
+    if args.v or args.V:
+        print("Program Version: 1.0")
 
 
 main()
