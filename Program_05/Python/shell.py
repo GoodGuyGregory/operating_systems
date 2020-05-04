eb3468eb5c1bc1e76e77ebd5f088e2f8480dc9d1
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-04 10:34:19 -0400

    initial program5 development

---
+++ Program_05/Python/shell.py
@@ -0,0 +1,57 @@
+import sys
+import os
+import fileinput
+import select
+import argparse
+
+# declare parser class
+parser = argparse.ArgumentParser(add_help=False)
+
+parser.add_argument('-v', '-V', help="displays version", action="store_true")
+parser.add_argument('-h', '-H', '-?', action='help',
+                    default=argparse.SUPPRESS, help='show help and usage.')
+parser.add_argument('--file', help="display drive contents", type=str)
+
+args = parser.parse_args()
+
+
+def readTheDisk(commandArgs):
+    # Checking for Stdin Pipe
+    if select.select([sys.stdin], [], [], 0.0)[0]:
+        # diskDrive = []
+        counter = 0
+        print("Displaying Drive Data: ")
+        for line in sys.stdin:
+            if counter == 0 or counter == 1:
+                counter += 1
+            else:
+
+                sys.stdout.write(line[2:])
+                counter += 1
+    else:
+        # Checks for File to use
+        fileName = args.file
+        counter = 0
+        print("Displaying Drive Data: ")
+        for fileName in fileinput.input():
+            # skip header lines of drive
+            if counter == 0 or counter == 1:
+                counter += 1
+
+            else:
+                sys.stdout.write(str(fileName[2:]))
+
+
+def main():
+    commands = sys.argv
+
+    # Checking for files or Arguments
+    if args.v:
+        print("Program Version: 1.0")
+    elif args.file:
+        readTheDisk(commands)
+    else:
+        readTheDisk(commands)
+
+
+main()
